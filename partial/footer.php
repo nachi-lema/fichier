505ac4073381df4e28a0c3879076a76445e19b80
nachi-lema <157596201+nachi-lema@users.noreply.github.com>
2024-12-27 00:55:35 +0100

    Add files via upload

---
+++ partial/footer.php
@@ -0,0 +1,113 @@
+    <!-- footer start -->
+    <footer class="footer-1 footer-3 overflow-hidden" style="background-image: url(assets/img/footer/footer-bg-3.png);">
+      <div class="overly">
+        <div class="container"></div>
+      </div>
+
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-md-6 col-xl-3">
+            <div class="single-footer-wid widget-description">
+              <a href="index.html" class="d-block mb-30 mb-xs-20">
+                <span class="text-white"> BAKUMBA EXPERT FIRM & CONSULTING</span>
+              </a>
+
+              <div class="description font-la color-white mb-40 mb-sm-30 mb-xs-25">
+                <p>"Bienvenue dans nos locaux ; qui sont d’une identité singulière… avec un design qui favorise la créativité, le bien-être et surtout pour vous recevoir dans les conditions que vous méritez parce que vous êtes précieux pour nous. </p>
+              </div>
+
+              <a href="#" class="theme-btn btn-red btn-md fw-600">En savoir plus <i class="far fa-chevron-double-right"></i></a>
+            </div>
+          </div>
+          <!-- /.col-lg-3 - single-footer-wid -->
+
+          <div class="col-md-6 col-xl-2">
+            <div class="single-footer-wid pl-xl-10 pl-50">
+              <h4 class="wid-title mb-30 color-white">Lien rapide</h4>
+              <ul>
+                <li><a href="about.php">À propos de nous</a></li>
+                <li><a href="#">domaines</a></li>
+                <li><a href="#">Notre équipe</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- /.col-lg-2 - single-footer-wid -->
+
+          <div class="col-md-6 col-xl-4">
+            <div class="single-footer-wid recent_post_widget pl-xl-10 pl-65 pr-50 pr-xl-30">
+              <h4 class="wid-title mb-30 color-white">Messages récents Post</h4>
+
+              <div class="recent-post-list">
+                <a href="#" class="single-recent-post mb-20 pb-20 d-flex align-items-center">
+                  <div class="thumb">
+                    <img src="assets/img/footer/resent-post-1.png" alt="">
+                  </div>
+
+                  <div class="post-data">
+                    <h5 class="color-white mb-10 fw-600">Why Purpose-Driven Employers Succeed</h5>
+                    <span class="color-white d-flex ailign-items-center"><i class="far fa-clock"></i>January 11, 2018</span>
+                  </div>
+                </a>
+
+                <a href="#" class="single-recent-post mb-20 pb-20 d-flex align-items-center">
+                  <div class="thumb">
+                    <img src="assets/img/footer/resent-post-2.png" alt="">
+                  </div>
+
+                  <div class="post-data">
+                    <h5 class="color-white mb-10 fw-600">Saving Time Achieving Success in Business</h5>
+                    <span class="color-white d-flex ailign-items-center"><i class="far fa-clock"></i>november 12, 2019</span>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <!-- /.col-lg-4 - single-footer-wid -->
+
+          <div class="col-md-6 col-xl-3">
+            <div class="single-footer-wid">
+              <h4 class="wid-title mb-30 color-white">Emplacement du bureau</h4>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31828.56309578647!2d15.25594615938712!3d-4.303279149100498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33c0fc32de97%3A0xa9c338eec9fbd094!2s48%20Av.%20Roi%20Beaudouin%2C%20Kinshasa!5e0!3m2!1sfr!2scd!4v1716163278942!5m2!1sfr!2scd" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+          <!-- /.col-lg-3 - single-footer-wid -->
+        </div>
+      </div>
+
+      <div class="footer-bottom overflow-hidden mt-20 mt-sm-15 mt-xs-10">
+        <div class="container">
+          <div class="footer-bottom-content d-flex flex-column flex-md-row justify-content-between align-items-center">
+            <div class="coppyright text-center text-md-start">
+              © 2024 <a href="index.php">Consulter</a> | Tous droits réservés par <a href="https://bakumba-expertfirm.cd/">BAKUMBA EXPERT FIRM & CONSULTING</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- footer end -->
+
+      <!--  ALl JS Plugins
+      ====================================== -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/scrollUp.min.js"></script>
+    <script src="assets/js/counterup.min.js"></script>
+    <script src="assets/js/jquery.sticky-kit.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.easypiechart.min.js"></script>
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/active.js"></script>
+
+    <!--<script src="https://translate.google.com/translate_a/element.js?cb=loadGoogleTranslate"></script>
+    <script type="text/javascript">
+      function loadGoogleTranslate() {
+        new google.translate.TranslateElement("google_element")
+      }
+    </script>--->
+  </body>
+
+</html>
---
+++ partial/header.php
@@ -0,0 +1,272 @@
+    <!-- header end -->
+    <header class="header header-1">
+      <div class="top-header theme-primary d-none d-md-block">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-8">
+              <div class="header-cta">
+                <ul>
+                  <li><a><i class="fal fa-clock"></i> Lun – Ven: 8.00 – 18.00</a></li>
+                  <li><a href="mailto:contact@bakumba-expertfirm.cd"><i class="icon-email"></i>contact@bakumba-expertfirm.cd</a></li>
+                  <li>
+                    <div id="google_element">
+                    
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="col-4">
+              <div class="header-right-socail d-flex justify-content-end align-items-center">
+                <h6 class="font-la color-white fw-600">Suivre sur :</h6>
+
+                <div class="social-profile">
+                  <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li>
+                      <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                          <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                        </svg>
+                      </a>
+                    </li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="main-header-wraper">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="d-flex align-items-center justify-content-between">
+                <div class="header-logo">
+                  <div class="logo">
+                    <a href="index.php">
+                      <img src="assets/img/logo/logo_.png" alt="logo">
+                    </a>
+                  </div>
+                </div>
+
+                <div class="header-menu d-none d-xl-block">
+                  <div class="main-menu">
+                    <ul>
+                      <li>
+                        <a href="index.php">Accueil</a>
+                      </li>
+                      <li>
+                        <a href="#">Domaines</a>
+                        <ul>
+                          <li><a href="#">Fiscalité et douane</a></li>
+                          <li><a href="#">Audit Organisationnel</a></li>
+                          <li><a href="#">Sous-traitance</a></li>
+                          <li><a href="#">Placement et pilotage des ressources humaines</a></li>
+                          <li><a href="facilitation.php">La facilitation financière et investissements </a></li>
+                          <li><a href="#">Conseil </a>
+                            <ul>
+                              <li><a href="#">Economie Bleue</a></li>
+                              <li><a href="#">Economie verte</a></li>
+                            </ul>
+                          </li>
+                          <li><a href="#">Mandataire en mines et carrières </a></li>
+                          <li><a href="#">Intervention en hydrocarbures </a></li>
+                          <li><a href="#">Fondation PSAUMES 35 </a></li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#">Pays</a>
+                        <ul>
+                          <li>
+                            <a href="">Emirats Arabes Unis</a>
+                            <ul>
+                              <li><a href="#">À propos de la filiale</a></li>
+                              <li><a href="#">Zones & domaines</a></li>
+                              <li><a href="#">Partenariats</a></li>
+                            </ul>
+                          </li>
+                          <li>
+                            <a href="#">France</a>
+                            <ul>
+                              <li><a href="#">À propos de la filiale</a></li>
+                              <li><a href="#">Zones & domaines</a></li>
+                              <li><a href="#">Partenariats</a></li>
+                            </ul>
+                          </li>
+                          <li>
+                            <a href="#">Etats Unis</a>
+                            <ul>
+                              <li><a href="#">À propos de la filiale</a></li>
+                              <li><a href="#">Zones & domaines</a></li>
+                              <li><a href="#">Partenariats</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="about.php">À propos de nous</a>
+                      </li>
+                      <li>
+                        <a href="#">Réalisations</a>
+                      </li>
+                      <li>
+                        <a href="news.php">Actualités</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div class="header-right d-flex align-items-center">
+                  <div class="horizontal-bar d-none d-md-block"></div>
+
+                 
+                    <div class="icon color-primary">
+                    
+                    
+                  
+                    </div>
+                    <div class="text">
+                      
+                    </div>
+                  <!--
+                  <div class="icon color-red">
+                          <i class="fal fa-map-marker-alt"></i>
+                        </div>
+                        <div class="text text-center">
+                            <span class="font-la mb-2 d-block fw-500 text-white"><u>Adresse physique</u></span>
+                            <span class="font-la mb-2 d-block fw-500 text-white">Résidence IMANI, Avenue Roi Baudouin N°48, <br> Kinshasa-GOMBE en <br> République Démocratique du Congo</span>
+                        </div>-->
+
+                  <div class="mobile-nav-bar d-block ml-3 ml-sm-5 d-xl-none">
+                    <div class="mobile-nav-wrap">
+                      <div id="hamburger">
+                        <i class="fal fa-bars"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- mobile menu - responsive menu  -->
+    <div class="mobile-nav">
+      <button type="button" class="close-nav">
+        <i class="fal fa-times-circle"></i>
+      </button>
+
+      <nav class="sidebar-nav">
+        <div class="navigation">
+          <div class="consulter-mobile-nav">
+            <ul>
+              <li>
+                <a href="index.php">Accueil</a>
+              </li>
+              <li>
+                <a href="#">Domaines</a>
+                <ul>
+                  <li><a href="#">Fiscalité et douane</a></li>
+                  <li><a href="#">Audit Organisationnel</a></li>
+                  <li><a href="#">Sous-traitance</a></li>
+                  <li><a href="#">Placement et pilotage des ressources humaines</a></li>
+                  <li><a href="facilitation.php">La facilitation financière et investissements </a></li>
+                  <li><a href="#">Conseil </a>
+                    <ul>
+                      <li><a href="#">Economie Bleue</a></li>
+                      <li><a href="#">Economie verte</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Mandataire en mines et carrières </a></li>
+                  <li><a href="#">Intervention en hydrocarbures </a></li>
+                  <li><a href="#">Fondation PSAUMES 35 </a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Pays</a>
+                <ul>
+                  <li>
+                    <a href="#">Emirats Arabes Unis</a>
+                    <ul>
+                      <li><a href="#">À propos de la filiale</a></li>
+                      <li><a href="#">Zones & domaines</a></li>
+                      <li><a href="#">Partenariats</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">France</a>
+                    <ul>
+                      <li><a href="#">À propos de la filiale</a></li>
+                      <li><a href="#">Zones & domaines</a></li>
+                      <li><a href="#">Partenariats</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Etats Unis</a>
+                    <ul>
+                      <li><a href="#">À propos de la filiale</a></li>
+                      <li><a href="#">Zones & domaines</a></li>
+                      <li><a href="#">Partenariats</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="about.php">À propos de nous</a>
+              </li>
+              <li>
+                <a href="#">Réalisations</a>
+              </li>
+              <li>
+                <a href="news.php">Actualités</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="sidebar-nav__bottom mt-20">
+            <div class="sidebar-nav__bottom-contact-infos mb-20">
+              <h6 class="color-black mb-5">Informations de contact</h6>
+              <ul>
+                <li><a><i class="fal fa-clock"></i> Lun – Ven: 8.00 – 18.00</a></li>
+                <li><a href="mailto:contact@bakumba-expertfirm.cd"><i class="icon-email"></i>contact@bakumba-expertfirm.cd</a></li>
+                <li>
+                  <a class="header-contact d-flex align-items-center">
+                    <div class="icon">
+                      <i class="icon-call"></i>
+                      <!-- <img src="assets/img/icon/phone-1.svg" alt=""> -->
+                    </div>
+                    <div class="text">
+                      <span class="font-la mb-5 d-block fw-500">Contacter pour obtenir de l'aide</span>
+                      <h5 class="fw-500">+243 812 909 639</h5>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="sidebar-nav__bottom-social">
+              <h6 class="color-black mb-5">Suivre sur :</h6>
+              <ul>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
